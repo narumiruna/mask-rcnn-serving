16292d731bb92c0425245f2ee4ff27c5715523bb
なるみ <weaper@gmail.com>
2018-07-20 13:49:28 +0800

    refactor code

--- detection_server.py
+++ detection_server.py
@@ -1,74 +1,72 @@
 import argparse
-import io
 import time
 from concurrent import futures
 
-import logging
-import cv2
 import grpc
-import numpy as np
-import tensorflow as tf
-from PIL import Image
 
 import serving_pb2
 import serving_pb2_grpc
 import utils
-from detector import CLASS_NAMES, Detector
+from detector import MaskRCNNDetector
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 
+def mrcnn_object_to_grpc_object(detected_object):
+    mask = detected_object.mask
+    sequence = utils.mask_to_polygon(mask)
+    if sequence is None:
+        return None
+
+    x, y, width, height = utils.roi_to_rect(detected_object.roi)
+
+    box = serving_pb2.Rectangle(x=x, y=y, width=width, height=height)
+    shape = serving_pb2.Shape(contentType='polygon/series', series=sequence)
+
+    grpc_object = serving_pb2.Object(
+        label=detected_object.label, box=box, shape=shape)
+
+    return grpc_object
+
+
+def mrcnn_objects_to_grpc_objects(detected_objects):
+    grpc_objects = []
+
+    for detected_object in detected_objects:
+        if detected_object is None:
+            continue
+        grpc_objects.append(detected_object)
+
+    return grpc_objects
+
+
 class ObjectDetection(serving_pb2_grpc.ObjectDetectionServicer):
 
-    def __init__(self, model_path='mask_rcnn_coco.h5'):
-        self._graph = tf.get_default_graph()
-        self._detector = Detector(model_path)
+    def __init__(self, detector):
+        self._detector = detector
 
     def Detect(self, request, context):
-        img = utils.load_bytes_image_array(request.image)
+        image = utils.load_bytes_image_array(request.image)
 
-        objects = []
+        mrcnn_objects = self._detector(image)
 
-        with self._graph.as_default():
-            r = self._detector.detect(img)
+        grpc_objects = mrcnn_objects_to_grpc_objects(mrcnn_objects)
 
-        class_ids = r['class_ids']
-        rois = r['rois']
-        masks_t = np.transpose(r['masks'], axes=(2, 0, 1))
-
-        for class_id, roi, mask in zip(class_ids, rois, masks_t):
-            sequence = utils.mask_to_polygon(mask)
-            if sequence is None:
-                continue
-
-            x, y, width, height = utils.roi_to_rect(roi)
-            box = serving_pb2.Rectangle(x=x, y=y, width=width, height=height)
-            shape = serving_pb2.Shape(
-                contentType='polygon/series', series=sequence)
-            objects.append(serving_pb2.Object(label=CLASS_NAMES[class_id], box=box, shape=shape))
-        return serving_pb2.DetectionResponse(type="objects", objects=objects)
+        return serving_pb2.DetectionResponse(
+            type="objects", objects=grpc_objects)
 
     def DetectStream(self, request, context):
-        img = utils.load_bytes_image_array(request.image)
+        image = utils.load_bytes_image_array(request.image)
 
-        with self._graph.as_default():
-            r = self._detector.detect(img)
+        mrcnn_objects = self._detector(image)
 
-        class_ids = r['class_ids']
-        rois = r['rois']
-        masks_t = np.transpose(r['masks'], axes=(2, 0, 1))
+        for mrcnn_object in mrcnn_objects:
+            grpc_object = mrcnn_object_to_grpc_object(mrcnn_object)
 
-        for class_id, roi, mask in zip(class_ids, rois, masks_t):
-            sequence = utils.mask_to_polygon(mask)
-            if sequence is None:
+            if grpc_object is None:
                 continue
 
-            x, y, width, height = utils.roi_to_rect(roi)
-            box = serving_pb2.Rectangle(x=x, y=y, width=width, height=height)
-            shape = serving_pb2.Shape(
-                contentType='polygon/series', series=sequence)
-            yield serving_pb2.Object(
-                label=CLASS_NAMES[class_id], box=box, shape=shape)
+            yield grpc_object
 
 
 def main():
@@ -79,8 +77,11 @@ def main():
     args = parser.parse_args()
 
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+    mask_rcnn_detector = MaskRCNNDetector(model_path=args.model)
+
     serving_pb2_grpc.add_ObjectDetectionServicer_to_server(
-        ObjectDetection(model_path=args.model), server)
+        ObjectDetection(detector=mask_rcnn_detector), server)
 
     address = '{}:{}'.format(args.host, args.port)
     server.add_insecure_port(address)
--- detector.py
+++ detector.py
@@ -3,11 +3,11 @@ import os
 import tempfile
 
 import numpy as np
-from PIL import Image
-
+import tensorflow as tf
 from mrcnn import model as modellib
 from mrcnn import utils
 from mrcnn.config import Config
+
 from utils import load_image_array
 
 CLASS_NAMES = [
@@ -34,11 +34,45 @@ class InferenceConfig(Config):
     NUM_CLASSES = 1 + 80
 
 
+class DetectedObject(object):
+    pass
+
+
+class MaskRCNNDetectedObject(DetectedObject):
+
+    def __init__(self, roi, class_id, score, mask):
+        self.roi = roi
+        self.class_id = class_id
+        self.score = score
+        self.mask = mask
+
+    @property
+    def label(self):
+        return CLASS_NAMES[self.class_id]
+
+    @property
+    def name(self):
+        return self.__class__.__name__
+
+    def __repr__(self):
+        return '{}, roi: {}, label: {}.'.format(self.name, self.roi, self.label)
+
+
 class Detector(object):
+
+    def __call__(self, image):
+        return self.detect(image)
+
+    def detect(self, image):
+        raise NotImplementedError
+
+
+class MaskRCNNDetector(Detector):
 
     def __init__(self, model_path, verbose=1):
         self._model_path = model_path
         self._verbose = verbose
+        self._graph = tf.get_default_graph()
 
         if not os.path.exists(self._model_path):
             utils.download_trained_weights(self._model_path)
@@ -48,8 +82,27 @@ class Detector(object):
                                         tempfile.TemporaryDirectory().name)
         self._model.load_weights(self._model_path, by_name=True)
 
-    def detect(self, img):
-        return self._model.detect([img], verbose=self._verbose)[0]
+    def detect(self, image):
+        """
+        Args:
+            image(ndarray): image to detect
+
+        Returns a list of MaskRCNNDetectedObject
+        """
+        with self._graph.as_default():
+            r = self._model.detect([image], verbose=self._verbose)[0]
+
+        rois = r['rois']
+        class_ids = r['class_ids']
+        scores = r['scores']
+        masks = np.transpose(r['masks'], axes=(2, 0, 1))
+
+        detected_objects = []
+        for roi, class_id, score, mask in zip(rois, class_ids, scores, masks):
+            obj = MaskRCNNDetectedObject(roi, class_id, score, mask)
+            detected_objects.append(obj)
+
+        return detected_objects
 
 
 def test():
@@ -58,13 +111,12 @@ def test():
     parser.add_argument('--image', type=str, default='example.jpg')
     args = parser.parse_args()
 
-    detector = Detector(args.model)
+    detector = MaskRCNNDetector(args.model)
     image = load_image_array(args.image)
-    r = detector.detect(image)
+    detected_objects = detector(image)
 
-    for i, (roi, class_id) in enumerate(zip(r['rois'], r['class_ids'])):
-        print('Object #{}, roi: {}, class: {}'.format(i + 1, roi,
-                                                      CLASS_NAMES[class_id]))
+    for detected_object in detected_objects:
+        print(detected_object)
 
 
 if __name__ == '__main__':
